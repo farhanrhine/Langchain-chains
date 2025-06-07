53e5c0906e91c3ec95251cfdb905ad1acf308965
farhanrhine <mohammadfarhanalam09@gmail.com>
2025-06-07 16:13:08 +0530

    build simple chain

--- simple_chain.py
+++ simple_chain.py
@@ -1,22 +1,22 @@
 from langchain_ollama import ChatOllama
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-load_dotenv()
+#load_dotenv()  # it is used to load the environment variables from the .env file when we are using the API key
 
 prompt = PromptTemplate(
     template='Generate 5 interesting facts about {topic}',
     input_variables=['topic']
 )
 
-model = ChatOllama(model_name='tinydolphin')
+model = ChatOllama(model='tinydolphin')
 
 parser = StrOutputParser()
 
-chain = prompt | model | parser
+chain = prompt | model | parser # pipe operaters it is used to connect the nodes
 
-result = chain.invoke({'topic':'cricket'})
+result = chain.invoke({'topic':'dubai'})
 
 print(result)
 
